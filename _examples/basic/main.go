631f2990504770b7ec126ebe074fca448effc51b
haashemi <mr.ali.haashemi@gmail.com>
2023-02-26 11:50:03 +0330

    chore(examples): update with framework changes

--- _examples/basic/main.go
+++ _examples/basic/main.go
@@ -16,33 +16,33 @@ func main() {
 		log.Fatalln(err.Error())
 	}
 
-	bot.OnMessage(filters.Text("hi"), Hi)
+	onMessage := bot.Party(filters.IsMessage())
+	{
+		onMessage.Handle(filters.Text("hi"), Hi)
 
-	bot.OnMessage(filters.True(), Echo)
+		onMessage.Handle(filters.True(), Echo)
+	}
 
 	log.Println("Bot is started as", bot.Username)
 	log.Fatalln(bot.StartPolling())
 }
 
 // Hi answers the hi message with a new hi!
-func Hi(ctx tgo.MessageContext) {
+func Hi(ctx tgo.Context) {
 	// Get sender's first name with getting the raw message
 	senderFirstName := ctx.Message().From.FirstName
 
 	// create the text using HTML Markups
 	text := fmt.Sprintf("Hi <i>%s</i>!", senderFirstName)
 
-	ctx.Send(text, &tgo.SendMessageOptions{
+	ctx.Reply(text, &tgo.SendMessageOptions{
 		// pass the parse mode, so telegram knows that our text contains HTML Markup!
 		ParseMode: tgo.ParseModeHTML,
 	})
 }
 
 // Echo just echoes with text
-func Echo(ctx tgo.MessageContext) {
-	// get text or caption of the sent message
-	text := ctx.Caption()
-
-	// send it back!
-	ctx.Send(text, nil)
+func Echo(ctx tgo.Context) {
+	// get text or caption of the sent message and send it back!
+	ctx.Send(ctx.Text(), nil)
 }
