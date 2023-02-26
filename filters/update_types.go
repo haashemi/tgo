9ba59c6aaecb03bc910d8e4adf26c08d7c6df690
haashemi <mr.ali.haashemi@gmail.com>
2023-02-26 11:37:09 +0330

    feat(filters): add update-based filters

---
+++ filters/update_types.go
@@ -0,0 +1,68 @@
+package filters
+
+import "github.com/haashemi/tgo"
+
+func HasMessage() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool {
+		return update.Message != nil ||
+			update.EditedMessage != nil ||
+			update.ChannelPost != nil ||
+			update.EditedChannelPost != nil
+	})
+}
+
+func IsMessage() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.Message != nil })
+}
+
+func IsEditedMessage() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.EditedMessage != nil })
+}
+
+func IsChannelPost() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.ChannelPost != nil })
+}
+
+func IsEditedChannelPost() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.EditedChannelPost != nil })
+}
+
+func IsInlineQuery() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.InlineQuery != nil })
+}
+
+func IsChosenInlineResult() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.ChosenInlineResult != nil })
+}
+
+func IsCallbackQuery() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.CallbackQuery != nil })
+}
+
+func IsShippingQuery() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.ShippingQuery != nil })
+}
+
+func IsPreCheckoutQuery() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.PreCheckoutQuery != nil })
+}
+
+func IsPoll() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.Poll != nil })
+}
+
+func IsPollAnswer() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.PollAnswer != nil })
+}
+
+func IsMyChatMember() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.MyChatMember != nil })
+}
+
+func IsChatMember() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.ChatMember != nil })
+}
+
+func IsChatJoinRequest() tgo.Filter {
+	return NewFilter(func(update *tgo.Update) bool { return update.ChatJoinRequest != nil })
+}
