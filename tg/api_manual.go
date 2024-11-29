a3975c7a55d516749af6f3fff3e3372c3e54dea8
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-11-30 02:14:59 +0330

    fix: merge extended api codes to api_manual file

--- tg/api_manual.go
+++ tg/api_manual.go
@@ -2,6 +2,33 @@ package tg
 
 import "io"
 
+// Username implements a string ChatID.
+type Username string
+
+// IsChatID does nothing but implements ChatID interface.
+func (Username) IsChatID() {}
+
+// ID implements a int64 ChatID.
+type ID int64
+
+// IsChatID does nothing but implements ChatID interface.
+func (ID) IsChatID() {}
+
+// ParseMode is a type that represents the parse mode of a message.
+// eg. Markdown, HTML, etc.
+type ParseMode string
+
+const (
+	// does not parse the message
+	ParseModeNone ParseMode = ""
+	// parses the message as Markdown
+	ParseModeMarkdown ParseMode = "Markdown"
+	// parses the message as Markdown but using telegram's V2 markdown
+	ParseModeMarkdownV2 ParseMode = "MarkdownV2"
+	// parses the message as HTML
+	ParseModeHTML ParseMode = "HTML"
+)
+
 // InputFile represents a file that can be used as input.
 type InputFile struct {
 	Value  string    // The value of the file (e.g., file ID, URL, or path)
